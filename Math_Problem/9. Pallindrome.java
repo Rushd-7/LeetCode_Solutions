e560b152bdb03be2adaa7828ea6bc04affdded04
Rushd-7 <rushdul2007@gmail.com>
2026-05-25 23:52:47 +0530

    Add 9. Pallindrome.java

---
+++ Math_Problem/9. Pallindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(int x) 
+    {
+        int rev=0;
+        int num=x;
+        if(x<0)
+        {
+            return false;
+        }
+        else
+        {
+            while(num>0)
+            {
+                rev=rev*10+(num%10);
+                num/=10;
+            }
+            if(x==rev)
+                return true;
+            else 
+                return false;
+        }
+    }
+}
