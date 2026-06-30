469874569e732051a8b773925594d35e53d131a2
Rushd ul Alam <rushdul2007@gmail.com>
2026-06-30 21:24:25 +0530

    Create 1486. XOR operation in an array.java

---
+++ Math_Problem/1486. XOR operation in an array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int xorOperation(int n, int start) {
+        int res=0;
+        int nums[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            nums[i]=start+2*i;
+            res=nums[i]^res;
+        }
+        return res;
+    }
+}
